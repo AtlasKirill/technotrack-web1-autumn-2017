f68790247659e052a28f261897dfc3031476da13
AtlasKirill <akimowkirill@gmail.com>
2017-09-24 15:57:55 +0300

    Update run.py

--- httpserver/server/run.py
+++ httpserver/server/run.py
@@ -1,27 +1,74 @@
 # -*- coding: utf-8 -*-
 import socket
+import os
+
+def parser_user_agent(string):
+	unuse,pattern,after_pat=string.partition("User-Agent: ")
+	del unuse, pattern
+	return after_pat[0:after_pat.find("\n")]
 
 
-def get_response(request):
+def parser_url(url,client_socket,request_string):
+	if url==1:
+		client_socket.send("""\
+    	HTTP/1.1 200 OK
 
-    return 'WRITE YOU RESPONSE HERE\n'
+    	Hello mister!
+    	You are: """+parser_user_agent(request_string)+"\n") 
+    	return
+	if url=="/test/":
+		client_socket.send(request_string)  #send string to the socket
+		return
+	if url=="/media/":
+		directory="/home/kirill/programms/progs_for_web/media"
+		files=os.listdir(directory)
+		for name in files:
+			client_socket.send(name+"\n")
+		return 
+	try:
+		file=open("."+url,'r')
+	except IOError as e:
+		client_socket.send("""\
+    	HTTP/1.1 404 Not Found
+
+		File not found\n""")
+	else:
+		client_socket.send(file.read())
+		
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def parser_request_dirs(request):
+	unuseble,pattern,after_pat=request.partition("GET ")
+	del pattern,unuseble
+	way=after_pat[0:after_pat.find("HTTP")-1]
+	if len(way)==1:
+		return 1
+	else:
+		return way
+
+
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 8000))  #
-server_socket.listen(0)  #
+server_socket.bind(('localhost', 8000))  #linking our socket with definite host and port
+server_socket.listen(5)  #turn on listening mode for socket with length of queue = 5
 
 print 'Started'
 
 while 1:
     try:
-        (client_socket, address) = server_socket.accept()
-        print 'Got new client', client_socket.getsockname()  #
-        request_string = client_socket.recv(2048)  #
-        client_socket.send(get_response(request_string))  #
+        (client_socket, adress) = server_socket.accept()
+        print 'Got new client', client_socket.getsockname(),adress  #return the socket's own adress (host,port)
+        request_string = client_socket.recv(2048)  #Receive data from the socket with 2048 size of buffer
+        #print request_string
+        #print parser_user_agent(request_string)
+        parser_url(parser_request_dirs(request_string),client_socket,request_string)
+
+
+          
         client_socket.close()
-    except KeyboardInterrupt:  #
+    except KeyboardInterrupt:  #if we try to stop prog with using "Ctrl+C", we have this exception
         print 'Stopped'
-        server_socket.close()  #
+        server_socket.close()  #Close the socket. All future operations on the socket object will fail
         exit()
